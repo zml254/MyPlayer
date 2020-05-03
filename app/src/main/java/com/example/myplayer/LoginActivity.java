e9a7520fa7457afee555063d5d38d319f538dfde
Bunke <2114347409@qq.com>
2020-05-03 22:38:00 +0800

    我是废物

--- app/src/main/java/com/example/myplayer/LoginActivity.java
+++ app/src/main/java/com/example/myplayer/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.myplayer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +15,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView login = (TextView) findViewById(R.id.activity_login_btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
